80b09d485efc7a85efd0fb6a383153bf50b63b92
David Stein <dstein@flatironinstitute.org>
2018-09-18 15:23:18 -0400

    optimizing laplace kernels

--- pybie2d/kernels/low_level/laplace.py
+++ pybie2d/kernels/low_level/laplace.py
@@ -33,11 +33,55 @@ def _LKANC(sx, sy, tx, ty, charge, pot):
     for i in range(tx.shape[0]):
         pot[i] = 0.0
     for i in numba.prange(tx.shape[0]):
+        temp = np.zeros(sx.shape[0])
         for j in range(sx.shape[0]):
             dx = tx[i] - sx[j]
             dy = ty[i] - sy[j]
-            d2 = dx**2 + dy**2
-            pot[i] += 0.5*np.log(d2)*charge[j]
+            temp[j] = dx**2 + dy**2
+        for j in range(sx.shape[0]):
+            temp[j] = np.log(temp[j])
+        for j in range(sx.shape[0]):
+            output[i] += charge[j]*temp[j]
+    for i in range(tx.shape[0]):
+        output[i] *= 0.5
+
+# @njit(parallel=True)
+# def lk_numba2(source, target, density):
+#     """
+#     2D Laplace Kernel (from source-->targets, no self-interaction testing)
+
+#     Pure Numba Implementation
+#     source:  array(2,N),  float
+#     targets: array(2,M),  float
+#     density: array(N),    float
+#     output:  array(M), float
+#     """
+#     N = source.shape[1]
+#     M = target.shape[1]
+#     s_x = source[0]
+#     s_y = source[1]
+#     t_x = target[0]
+#     t_y = target[1]
+#     scale = -0.25/np.pi
+#     output = np.zeros(M)
+#     # now the main kernel call
+#     for i in prange(M):
+#         tx = t_x[i]
+#         ty = t_y[i]
+#         temp = np.zeros(N)
+#         for j in range(N):
+#             dx = s_x[j] - tx
+#             dy = s_y[j] - ty
+#             temp[j] = dx**2 + dy**2
+#         for j in range(N):
+#             temp[j] = np.log(temp[j])
+#         for j in range(N):
+#             output[i] += density[j]*temp[j]
+#     # scale the output
+#     for i  in range(M):
+#         output[i] *= scale
+#     return output
+
 
 @numba.njit(parallel=True)
 def _LKANCG(sx, sy, tx, ty, charge, pot, gradx, grady):
